f39eba49cb1d1b0a5c093f6cfd786b4c8c97448b
Caro Tobar <carotobar@Caros-MacBook-Air.local>
2019-12-04 23:06:13 -0500

    User input boxes now responsive

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
 // generate password
 
+
 function generate(){
 
  var length = document.getElementById("slider").value;
+ var numBox = document.getElementById("numbersBox").checked;
+ var specBox = document.getElementById("specChar").checked;
+ var lowerBox = document.getElementById("lowerBox").checked;
+ var upperBox = document.getElementById("upperBox").checked;
+
+
 
 //possible password values
 
- var values ="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~!#$%^&*?/()+_-{}";
+var values = "";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numbers = "1234567890";
+var special = "~!#$%^&*?/()+_-{}";
 
+// if uppercaseBox is checked, values = values + uppercase
+
+////creating checkbox functions responsive to user input
+
+if (numBox === true) {
+    values = values + numbers;
+   
+}
+
+if (specBox === true) {
+    values = values + special;
+    
+}
+
+if (lowerBox === true) {
+    values = values + lowercase;
+}
+
+if (upperBox === true) {
+    values = values + uppercase;
+}
+
+// and so on for others
 
   var password ='';
 
   /// creating for loop for password characters
 
-  for(var i = 1; i <= length; i++) {
+  for(var i = 0; i < length; i++) {
       password = password + values.charAt(Math.floor(Math.random() * (values.length - 1)));
   }
     /// add password to input box
@@ -45,7 +79,5 @@ function copyToClipBoard(){
     document.execCommand("copy");
     } 
 
-    
-////creating checkbox functions responsive to user input
 
 
